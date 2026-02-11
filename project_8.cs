69041b53bb2b6971114dcc5a53f1f46e05f1f2ab
Aditya Sasodey <asasodey21@gmail.com>
2026-02-11 09:42:48 +0530

    Implement loop for repeated bill calculations and added string interpolation
    Added a loop to allow multiple calculations based on user input.

--- project_8.cs
+++ project_8.cs
@@ -2,6 +2,8 @@
     Console.WriteLine("Hello World");
     Console.WriteLine("this is a simple bill calculator");
 
+	string continueChoice;
+	do{
     Console.Write("enter quantity: ");
     string userQuantity = Console.ReadLine();
     int finalQuantity = Convert.ToInt32(userQuantity);
@@ -16,15 +18,17 @@
     double taxAmount = (double) subTotal*(amount/100);
     double finalPrice = (double) subTotal + taxAmount;
 		
-    if ((subTotal >= 2000) && (taxAmount >= 18))
+    if ((subTotal >= 2000) && (taxAmount >= (subTotal * 0.18)))
 		{
 		finalPrice += 100;
 			Console.WriteLine("Surcharge of Rs 100/- is added to your purchase");
 		}
-		else if ((subTotal < 2000) || (taxAmount < 18))
+		else
 		{
 			Console.WriteLine("No Surcharge will be added to your purchase");
 		}
-		
-    Console.WriteLine("your final is " + subTotal + ", and your tax is " + taxAmount + ", and your final price is " + finalPrice);
+    Console.WriteLine($"your final is {subTotal}, and your tax is {taxAmount}, and your final price is {finalPrice}");
+
+		Console.WriteLine("y/n");
+	}while (continueChoice == "y");
 }
