b31b352c51a362400063606406ea807622200a25
Aditya Sasodey <asasodey21@gmail.com>
2026-06-15 21:01:06 +0530

    Add project files.

---
+++ BlazorBillingAppV1/BlazorBillingAppV1.Client/Models/BlazorClass.cs
@@ -0,0 +1,41 @@
+﻿namespace BlazorBillingAppV1.Client.Models
+{
+    public class BillingInvoice
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public double Tax { get; set; }
+        public bool Surcharge { get; private set; }
+
+        public double CalculateSubTotal()
+        {
+            return Quantity * Price;
+        }
+
+        public double CalculateTaxAmount()
+        {
+            return CalculateSubTotal() * (Tax / 100);
+        }
+
+        public double FinalPrice()
+        {
+            return CalculateSubTotal() + CalculateTaxAmount();
+        }
+
+        public double FinalDisplay()
+        {
+            double finalPrice = FinalPrice();
+
+            if ((CalculateSubTotal() >= 2000) && (CalculateTaxAmount() >= (CalculateSubTotal() * 0.18)))
+            {
+                finalPrice += 100;
+                Surcharge = true;
+            }
+            else
+            {
+                Surcharge = false;
+            }
+            return finalPrice;
+        }
+    }
+}
